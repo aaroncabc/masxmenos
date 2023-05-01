9f47368c978ea92a1dda03fce4340785a1c6b61a
aaroncabc <aaroncabrales35@gmail.com>
2023-05-01 18:31:36 -0500

    Login/register forms

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('productos/<int:id>/<str:order>/<str:cat>', views.productosn, name='productosn'),
     path('productos/<int:id>/<str:store>/<str:order>/<str:cat>', views.storeproductosn, name='storeproductosn'),
+    path('login',views.login,name='login'),
+    path('register',views.register ,name='register')
+
 ]
 
--- catalog/views.py
+++ catalog/views.py
@@ -3,6 +3,11 @@ from .models import Producto, Tienda,Categoria
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+def login(request):
+    return render(request,'login.html')
+
+def register(request):
+    return render(request,'register.html')
 
 def productosn(request,id,order,cat):
 
